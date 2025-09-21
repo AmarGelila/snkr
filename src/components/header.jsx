77bcd10bc14784529d491a38aa5ece8a42132908
AmarGelila <merogelila@gmail.com>
2025-09-21 15:30:56 +0300

    Last Commit

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons/faCartShopping';
 import { useCartItems } from '../stores';
 import { useEffect, useState } from 'react';
-
+import { Link } from 'react-router-dom';
 function Header() {
 	const [currentRoute, setCurrentRoute] = useState('/');
 	const totalQuantity = useCartItems((state) => state.totalQuantity);
@@ -15,25 +15,25 @@ function Header() {
 			<div className="px-4">
 				<ul className="flex flex-wrap items-center justify-center gap-3">
 					<li className="me-0 basis-full px-3 pt-5 pb-0 sm:me-auto sm:basis-auto sm:pt-3 sm:pb-3">
-						<a href="/" className="flex justify-center sm:inline">
+						<Link to="/" className="flex justify-center sm:inline">
 							<img src="/header-logo.svg" alt="Website Logo" />
-						</a>
+						</Link>
 					</li>
 					<li className="p-3 uppercase">
-						<a href="/">home</a>
+						<Link to="/">home</Link>
 					</li>
 					<li className="p-3 uppercase">
-						<a href="/products">products</a>
+						<Link to="/products">products</Link>
 					</li>
 					<li className="p-3 uppercase">
-						<a href="/cart" className="relative text-2xl">
+						<Link to="/cart" className="relative text-2xl">
 							<span
 								className={`absolute ${currentRoute === '/cart' || totalQuantity === 0 ? 'hidden' : ''} start-8/10 top-1/4 flex h-7 w-7 -translate-1/2 items-center justify-center ${totalQuantity !== 0 ? 'animate-bounce' : ''} rounded-full bg-red-600 p-2 text-white`}
 							>
 								{totalQuantity}
 							</span>
 							<FontAwesomeIcon icon={faCartShopping} />
-						</a>
+						</Link>
 					</li>
 				</ul>
 			</div>
